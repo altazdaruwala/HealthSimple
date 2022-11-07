d6c1503a850c61fc23a1f5beb31facceb170187f
vir8 <viratpatel10101998@gmail.com>
2022-11-06 23:14:04 -0500

    coonecting to AWS S3

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "healthsimple3dff6c40": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3ce66de60": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
